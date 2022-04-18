c310a5b53b6e47e2b928a7ee1d75b627f8359f60
Rabeebaqdas <MuhammadAdeelUsman>
2022-04-18 14:10:31 +0500

    this is the 40th project from 50 project

--- src/App.js
+++ src/App.js
@@ -6,37 +6,36 @@ import React,{useRef,useEffect,useState} from 'react';
 
 function App() {
 
-    const password = useRef(null)
-    const background = useRef(null)
-
+    const [count,setCount] = useState(false)
     useEffect(()=>{
-        password.current.addEventListener('input', (e) => {
-            const input = e.target.value
-            const length = input.length
-            const blurValue = 20 - length * 2
-            background.current.style.filter = `blur(${blurValue}px)`
-        })
+        const boxesContainer = document.getElementById('boxes')
+
+    
+        function createBoxes() {
+            for (let i = 0; i < 4; i++) {
+                for (let j = 0; j < 4; j++) {
+                    const box = document.createElement('div')
+                    box.classList.add('box')
+                    box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
+                    boxesContainer.appendChild(box)
+                }
+            }
+        }
+        
+        createBoxes()
+
     },[])
+
+    const handleChange = () => {
+        setCount(!count);
+    }
 
 
   return (
     <>
-    <div class="background" id="background" ref={background}></div>
-    <div class="bg-white rounded p-10 text-center shadow-md">
-        <h1 class="text-3xl">Image Password Strength</h1>
-        <p class="text-sm text-gray-700">Change the password to see the effect</p>
-        <div class="my-4 text-left">
-            <label for="email" class="text-gray-900">Email:</label>
-            <input type="text" class="border block w-full p-2 mt-2 rounded" id="email" placeholder="Enter Email" />
-        </div>
-        <div class="my-4 text-left">
-            <label for="password" class="text-gray-900">Password:</label>
-            <input type="password" class="border block w-full p-2 mt-2 rounded" id="password" ref={password}
-                placeholder="Enter Password" />
-        </div>
+    <button className="magic" id="btn" onClick={handleChange}>Magic 🎩</button>
+    <div className={`boxes ${count ? "big" : '' }`} id="boxes"></div>
 
-        <button class="bg-black text-white py-2 mt-4 inline-block w-full rounded" type="submit">Submit</button>
-    </div>
     </>
   )
 }
