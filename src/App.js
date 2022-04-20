5b4574dad5498469cdd8128a514f243150250b61
Rabeebaqdas <MuhammadAdeelUsman>
2022-04-20 14:43:25 +0500

    this is the 46th project from 50 projects

--- src/App.js
+++ src/App.js
@@ -5,56 +5,151 @@ import React,{useRef,useEffect,useState} from 'react';
 
 
 function App() {
-
-    const [count,setCount] = useState(false);
-    const open = () => {
     
-        setCount(true);
- 
+   
+    const a = useRef(null)
+    const b = useRef (null)
+    const c = useRef (null)
+    const d= useRef (null)
+    const quiz = useRef(null)
+    const question = useRef (null)
+    const submit = useRef(null)
+    const answer = useRef(null)
+    
+    let score = 0;
+    let currentQuiz = 0;
+
+    const quizData = [
+        {
+            question: "which language runs in a web browser?",
+            a: "java",
+            b: "c",
+            c: "python",
+            d:"javascript",
+            correct: "d",
+        },
+        {
+            question: "what does css stand for?",
+            a: "central style shetts",
+            b: "cascading style sheets",
+            c:"cars suvs sailboats",
+            d:"cascading simple sheets",
+            correct: "b"
+        },
+        {
+            question: "what does Html stand for?",
+            a: "central style shetts",
+            b: "cascading style sheets",
+            c:"Hypertext markup language",
+            d:"helicopters terminals motorboats lamborginis",
+            correct: "c"
+        },
+        {
+            question: "what was the year when javascript launched?",
+            a: "1996",
+            b: "1995",
+            c:"1994",
+            d:"none of the above",
+            correct: "b"  
+        }
+    
+    ]
+
+    useEffect(()=>{
+        loadQuiz()
+    },[])
+
+
+    const loadQuiz = ()=>{
+        deselectAnswers()
+        const currentQuizData = quizData[currentQuiz];
+        question.current.innerText = currentQuizData.question;
+        a.current.innerText=currentQuizData.a
+        b.current.innerText=currentQuizData.b;
+        c.current.innerText=currentQuizData.c;
+        d.current.innerText=currentQuizData.d;
+
+
     }
-    const close = () => {
-     
-        setCount(false);
+    const deselectAnswers=()=>{
+        let answerEle = document.querySelectorAll('.answer')
+        answerEle.forEach((element)=>{
+            element.checked = false;
+        })
+      
     }
 
 
+   
 
+    const getSelected = () =>{
+        let answer;
+        let answerEle = document.querySelectorAll('.answer')
+        
+        answerEle.forEach((answerEle)=>{
+            if(answerEle.checked){
+                answer=answerEle.id;
+            }
+        })
+        return answer;
+    }
+
+    const handleChange = () =>{
+        const answer = getSelected();
+
+        if(answer){
+            if(answer===quizData[currentQuiz].correct){
+                score++;
+            }
+            currentQuiz++;
+            if(currentQuiz < quizData.length){
+                loadQuiz()
+            }
+            else{
+                quiz.current.innerHTML = `<h2>You answered  ${score} / ${quizData.length}
+                questions corretly</h2> 
+                <button onClick='location.reload()'>Reload</button>
+                `
+            }
+        }
+
+    }
   return (
-    <>
-    <button className="nav-btn open-btn"  onClick={open}>
-        <i className="fas fa-bars"></i>
-    </button>
-    <img src="https://www.freepnglogos.com/uploads/netflix-logo-0.png"  className='logo' />
-    <p className="text">Mobile Navigation</p>
 
-    <div className={`nav nav-black ${count ? "visible" : ""}`}>
-        <div className={`nav nav-red ${count ? "visible" : ""}`}>
-            <div className={`nav nav-white ${count ? "visible" : ""}`}>
-                <button className="nav-btn close-btn">
-                    <i className="fas fa-times" onClick={close}></i>
-                </button>
-                <img src="https://www.freepnglogos.com/uploads/netflix-logo-0.png"  className='logo' />
-                <ul className="list">
-                    
-                <li><a href="#">Teams</a></li>
-                <li><a href="#">Locations</a></li>
-                <li><a href="#">Life at Netflix</a></li>
-                <li>
-                    <ul>
-                        <li><a href="#">Netflix culture memo </a></li>
-                        <li><a href="#">Work life balance </a></li>
-                        <li><a href="#">Inclusion & diversity </a></li>
-                        <li><a href="#">Blog</a></li>
-                        
-                    </ul>
-                </li>
+        <div className="quiz-container" id="quiz"  ref={quiz}>
+            <div className="quiz-header">
+                <h2 id="question" ref={question}>Question text</h2>
+                <ul>
+                    <li>
+                        <input type="radio" name="answer" id="a" 
+                        className='answer' ref={answer}/>
+                        <label htmlFor="a" id='a_text' ref={a}>Question</label>
+                    </li>
+
+                    <li>
+                        <input type="radio" name="answer" id="b" 
+                        className='answer' ref={answer}/>
+                        <label htmlFor="b" id='b_text' ref={b}>Question</label>
+                    </li>
+
+                    <li>
+                        <input type="radio" name="answer" id="c"
+                        className='answer' ref={answer}/>
+                        <label htmlFor="c" id='c_text' ref={c}>Question</label>
+                    </li>
+
+                    <li>
+                        <input type="radio" name="answer" id="d"
+                        className='answer' ref={answer}/>
+                        <label htmlFor="d" id='d_text' ref={d}>Question</label>
+                    </li>
                 </ul>
             </div>
+            <button id='submit' ref={submit} onClick={handleChange}>Submit</button>
         </div>
-    </div>
-    
-</>
+ 
   )
 }
 
 export default App;
+
